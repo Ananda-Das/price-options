c0b2111050fa30ab87ffd74f91534f37a3838359
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-09-18 08:00:09 +0600

    use reachart to draw any type of chart using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import LineChart from './components/LineChart/LineChart'
 import Navbar from './components/Navbar/Navbar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 // import DauisyNav from './components/dauisy-nav/DauisyNav'
@@ -11,6 +12,7 @@ function App() {
       {/* <DauisyNav></DauisyNav> */}
       <h1>Vite + React</h1>
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
     </>
   )
 }
